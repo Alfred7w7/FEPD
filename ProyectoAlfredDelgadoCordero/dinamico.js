6ac69c9c02b2869c11aa1ae45f5895c214591a45
Alfred7w7 <104035426+Alfred7w7@users.noreply.github.com>
2022-06-07 16:03:24 -0300

    Add files via upload

---
+++ ProyectoAlfredDelgadoCordero/dinamico.js
@@ -0,0 +1,9 @@
+function inclusive() {
+    document.body.style.backgroundColor= "black";
+    document.getElementById("header").style.background = "white";
+    document.getElementById("texto").style.color = "white";
+   
+}
+function copy() {
+    alert("Autor: Alfred Delgado Cordero. EMAIL: alfreddeco777@gmail.com");
+    }
